92e69e608ea7aa911a7045cd46ea652b1d569f4f
Hazael Lopez <elhazael134@gmail.com>
2022-10-19 11:39:37 -0600

    LoginPage - Diseño sin Layout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -3,8 +3,9 @@ import { Typography } from "@mui/material"
 export const JournalPage = () => {
   return (
     <>
-        <Typography component='h1'>JournalPage</Typography>
-        <hr/>
+        <Typography component='h1'>
+          JournalPage
+        </Typography>
     </>
   )
 }
