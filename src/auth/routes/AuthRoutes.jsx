76c590f59e285c8ed04d4e0b0cd3f0df00143d64
Hazael Lopez <elhazael134@gmail.com>
2022-10-23 10:20:31 -0600

    Checking Authentication

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -1,8 +1,15 @@
+import { useSelector } from "react-redux"
 import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage,
          RegisterPage } from "../"
+import { CheckingAuth } from "../../ui";
 
 export const AuthRoutes = () => {
+
+  const {status} = useSelector(state => state.auth);
+
+  if(status === 'checking') return <CheckingAuth/>
+
   return (
     <>
         <Routes>
