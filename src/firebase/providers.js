9da4b51e0fba983b3e52319b1760c4cd1dd3c5f1
Hazael Lopez <elhazael134@gmail.com>
2022-10-23 11:51:31 -0600

    Agregamos nuestro directorio firebase excluyento únicamente nuestro archivo config.js

---
+++ src/firebase/providers.js
@@ -0,0 +1,56 @@
+import { createUserWithEmailAndPassword, 
+         GoogleAuthProvider, 
+         signInWithPopup,
+         updateProfile, 
+         signInWithEmailAndPassword } from "firebase/auth";
+import { FirebaseAuth } from "./config";
+
+const googleProvider = new GoogleAuthProvider();
+
+export const signInWithGoogle = async () => {
+    try {
+        const result = await signInWithPopup(FirebaseAuth, googleProvider);
+        // const credential = GoogleAuthProvider.credentialFromResult(result);
+        const {displayName, email, photoURL, uid} = result.user;
+
+        return { ok: true, displayName, email, photoURL, uid }
+
+    } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        return { ok: false, errorMessage, }
+    }
+}
+
+export const registerUserWithEmailPassword = async ({email, password, displayName}) => {
+    try {
+        const resp = await createUserWithEmailAndPassword(FirebaseAuth, email, password);
+        const { uid, photoURL } = resp.user;
+        // TODO: actualizar el displayName en Firebase
+        await updateProfile(FirebaseAuth.currentUser, {displayName});
+
+        return { ok: true, uid, photoURL, email, displayName}
+        
+    } catch (error) {
+        console.log(error);
+        return { ok: false, errorMessage: error.message}
+    }
+}
+
+export const loginWithEmailAndPassword = async ({email, password}) => {
+    //! singinwithemailandpassword
+    try {
+        const resp = await signInWithEmailAndPassword(FirebaseAuth, email, password);
+        const {uid, photoURL, displayName} = resp.user;
+        
+        return {ok: true, uid, photoURL, email, displayName}
+    } catch (error) {
+        console.log(error);
+        return { ok: false, errorMessage: error.message}
+    }
+}
+
+export const logoutFirebase = async () => {
+    return await FirebaseAuth.signOut();
+}
